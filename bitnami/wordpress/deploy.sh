3e7a1771f4426f782fee2c186a095d423c434808
Ying Huang <huangyingw@gmail.com>
2021-04-19 22:21:07 -0700

    n

--- bitnami/wordpress/deploy.sh
+++ bitnami/wordpress/deploy.sh
@@ -6,3 +6,14 @@ cd "$SCRIPTPATH"
 #helm dependency update ./
 helm uninstall wordpress
 helm install wordpress ./
+
+'
+kubectl get persistentvolumeclaims
+NAME                       STATUS    VOLUME   CAPACITY   ACCESS MODES   STORAGECLASS   AGE
+data-wordpress-mariadb-0   Pending                                                     11h
+kubectl get persistentvolumes
+No resources found in default namespace.
+
+../mariadb/templates/master-statefulset.yaml:321:          persistentVolumeClaim:
+templates/deployment.yaml:228:          persistentVolumeClaim:
+'
