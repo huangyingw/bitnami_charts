8ad6f35d53027215609a017eca5e605957146d4f
Ying Huang <huangyingw@gmail.com>
2021-04-20 21:59:01 -0700

    n

--- bitnami/mariadb/deploy.sh
+++ bitnami/mariadb/deploy.sh
@@ -4,6 +4,8 @@ SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
 helm uninstall mariadb
+helm dependency update ./
 # helm install mariadb ./ --set master.persistence.storageClass=gluster-heketi,slave.persistence.storageClass=gluster-heketi
+helm install mariadb ./ --set global.storageClass=openebs-hostpath
 export MY_PASSWORD=my-pass
-helm install mariadb ./ --values ./custom-values.yaml --set db.password=$MY_PASSWORD
+# helm install mariadb ./ --values ./custom-values.yaml --set db.password=$MY_PASSWORD
