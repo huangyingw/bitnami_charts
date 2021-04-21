67a29bfb3851819faf805a8e15a313ef87ca2ead
Ying Huang <huangyingw@gmail.com>
2021-04-20 21:59:29 -0700

    n

--- bitnami/wordpress/deploy.sh
+++ bitnami/wordpress/deploy.sh
@@ -3,9 +3,10 @@ SCRIPT=$(realpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
-#helm dependency update ./
+# helm dependency update ./
 helm uninstall wordpress
-helm install wordpress ./
+helm install wordpress ./ --set global.storageClass=openebs-hostpath
+# helm install wordpress ./ --set global.storageClass=openebs-hostpath,mariadb.primary.persistence.storageClass=openebs-hostpath
 
 '
 kubectl get persistentvolumeclaims
